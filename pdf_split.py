0336d95887994a98fa85a8e8e60c438afd4d6842
Jason Toney <toneyja@oregonstate.edu>
2023-03-12 01:37:36 -0800

    pdf_split.py: reformat code to use PyPDF2 instead of GhostScript

--- pdf_split.py
+++ pdf_split.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-"""Use Ghostscript to split a pdf file into individual sections.
+"""Use PyPDF2 to split a pdf file into individual sections.
 
-This script assumes that you have python3, the python3-magic and python3-numpy
-modules, and Ghostscript installed.
+This script assumes that you have python3, the python3-magic and python3-pypdf2
+modules installed.
 
 Accept a pdf file as input and split it into individual pdf files containing
 each part, using section names and page number boundaries defined by the user.
 
 There must be a file with the same name as the source pdf file with the file
-extension ".pages" in the same directory as the source file, eg: a source file
-named Book.pdf needs a corresponding ".pages" file named Book.pdf.pages. The
-".pages" file should contain the name of each section with the first and last
+extension ".csv" in the same directory as the source file, eg: a source file
+named Book.pdf needs a corresponding ".csv" file named Book.csv. The
+".csv" file should contain the name of each section with the first and last
 pages of that section in the following format:
 
-Chapter-1, 1, 20
-Chapter-2, 21, 45
-Chapter-3, 46, 75
-Glossary, 76, 80
+Chapter-1,1,20
+Chapter-2,21,45
+Chapter-3,46,75
+Glossary,76,80
 """
 
 
-from os import path, system
+from os import path
+import csv
 import sys
 
 from magic import from_file
-from numpy import genfromtxt
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
 def pdf_split(pdf_source_file):
-    """Use Ghostscript to split a pdf file into individual parts."""
+    """Use PyPDF2 to split a pdf file into individual parts."""
     if not path.isfile(pdf_source_file):
         sys.exit(f"The pdf file {pdf_source_file} does not exist!")
 
@@ -38,50 +39,36 @@ def pdf_split(pdf_source_file):
     if "PDF document" not in file_type:
         sys.exit(f"The file {pdf_source_file} is not a valid pdf file!")
 
-    pages_file = pdf_source_file + ".pages"
+    pages_file = f"{path.splitext(path.basename(pdf_source_file))[0]}.csv"
     if not path.isfile(pages_file):
         sys.exit(f"The pages file {pages_file} does not exist!")
 
     print(f"Splitting pdf file: {pdf_source_file}")
 
-    try:
-        pages = genfromtxt(pages_file, dtype="str", delimiter=", ")
-    except ValueError as exception:
-        print(f"The file {pages_file} is not a valid pages file!")
-        sys.exit(exception)
+    with open(pages_file, "r") as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            pdf_destination_file = row[0]
+            first_page = int(row[1])
+            last_page = int(row[2])
 
-    index = 0
-    while index < len(pages):
-        pdf_destination_file = pages[index][0] + ".pdf"
-        first_page = pages[index][1]
-        last_page = pages[index][2]
-        index += 1
+            if path.isfile(pdf_destination_file):
+                print(f"The pdf file {pdf_destination_file} already exists.")
+                continue
 
-        if path.isfile(pdf_destination_file):
-            print(f"The pdf file {pdf_destination_file} already exists.")
-            continue
+            print(
+                f"Splitting pages {first_page} to {last_page} into file: {pdf_destination_file}"
+            )
 
-        print(
-            "Splitting pages "
-            + first_page
-            + " to "
-            + last_page
-            + " into file: "
-            + pdf_destination_file
-        )
+            with open(pdf_source_file, "rb") as input_file:
+                pdf_reader = PdfFileReader(input_file)
+                output_pdf = PdfFileWriter()
 
-        system(
-            "gs -sDEVICE=pdfwrite -dNOPAUSE -dBATCH -dSAFER -q"
-            + " -dFirstPage="
-            + first_page
-            + " -dLastPage="
-            + last_page
-            + " -sOutputFile="
-            + pdf_destination_file
-            + " "
-            + pdf_source_file
-            + " 2>/dev/null"
-        )
+                for page_num in range(first_page - 1, last_page):
+                    output_pdf.addPage(pdf_reader.getPage(page_num))
+
+                with open(f"{pdf_destination_file}.pdf", "wb") as output_file:
+                    output_pdf.write(output_file)
 
 
 if __name__ == "__main__":
